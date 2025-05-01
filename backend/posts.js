b4676ed421af4e1650558251c91fed620014eef1
Ryan <115543058+WareMoss@users.noreply.github.com>
2025-05-01 12:35:11 +0100

    Update posts.js

--- backend/posts.js
+++ backend/posts.js
@@ -1,1 +1,18 @@
+// NOTE TO SELF:
+// not every line is commented as if I have explained it before
+// typing it again will take a while 
 
+
+const Router = require('@koa/router');
+const { validateBody, postSchema } = require('./validate');
+const poolPromise = require('./config');
+const requireScope = require('./scopeAuth');
+const router = new Router();
+
+router.post('/user/posts', requireScope('posts:write'), validateBody(postSchema), async (ctx) => {
+  // create a new post
+  // protected so must have an account to make 
+  // validated by validate middleware which checks the request adheres to rules made in validate
+  const { id: userId } = ctx.state.user || {};
+  // gets userid from decoded JWT token 
+  // if undefined, make empty 

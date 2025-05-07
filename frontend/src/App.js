c6f09b29f6b548c33d3aa8bc4696a74c67d57c6c
Ryan <115543058+WareMoss@users.noreply.github.com>
2025-05-07 14:33:48 +0100

    Update App.js
    Added imports needed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,1 +1,10 @@
-
+import React, { useState, useEffect } from 'react';
+import { Route, Routes, Link, useNavigate } from 'react-router-dom';
+import { Container, Navbar, Nav, Button } from 'react-bootstrap';
+import Login from './components/Login';
+import Register from './components/Register';
+import Posts from './components/Posts';
+import PostDetail from './components/PostDetail';
+import Profile from './components/Profile';
+import ProtectedRoute from './components/protectedRoute';
+// imports all routing components 

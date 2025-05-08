0f5495f3c10526278e4ecf064a36cebb7e1ce0e9
Ryan <115543058+WareMoss@users.noreply.github.com>
2025-05-08 12:12:12 +0100

    Update Comments.js

--- frontend/src/components/Comments.js
+++ frontend/src/components/Comments.js
@@ -1,1 +1,7 @@
-
+import React, { useEffect, useState, useCallback } from 'react';
+import { Card, Button, Alert, Spinner } from 'react-bootstrap';
+import api from '../services/api';
+// import react and hooks from react library 
+// useEffect = run code when component mounts or updates 
+// useState = manage data inside component 
+// useCallback = remember functions so they arent created ever time its rendered 

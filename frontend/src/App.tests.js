b5053178f1dfb62b40c0353c2409499c985eb0e0
Ryan <115543058+WareMoss@users.noreply.github.com>
2025-05-08 12:07:56 +0100

    Update App.tests.js

--- frontend/src/App.tests.js
+++ frontend/src/App.tests.js
@@ -1,1 +1,11 @@
+//App.test.js:
+import { render, screen } from '@testing-library/react';
+import App from './App';
 
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+// the goal of this is to define test cases using the test function 

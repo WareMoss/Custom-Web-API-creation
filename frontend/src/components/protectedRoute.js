e93fbcfa875fd3749a1f7462172c524ef5e4eefc
Ryan <115543058+WareMoss@users.noreply.github.com>
2025-05-20 10:58:17 +0100

    Update protectedRoute.js

--- frontend/src/components/protectedRoute.js
+++ frontend/src/components/protectedRoute.js
@@ -1,1 +1,14 @@
+import React from 'react';
+import { Navigate } from 'react-router-dom';
 
+const ProtectedRoute = ({ children }) => {
+  // takes children parameter, this represents the component 
+  // that should be rendered if the user is authenticated 
+  const isAuthenticated = localStorage.getItem('access_token'); 
+  // check the user is authenticated by looking for this token in local storage 
+  // if it exists, isAuthenticated will be true 
+  // if not false 
+  return isAuthenticated ? children : <Navigate to="/login" />;
+};
+
+export default ProtectedRoute;
